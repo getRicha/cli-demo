910d3857cd6e0a0687d2cec34ddd544ac920a776
getRicha <richasri2002@gmail.com>
2022-01-20 00:53:34 +0530

    Add chalk and boxen

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
+const chalk=require('chalk');
+const boxen=require('boxen');
 
-console.log(`Name: Richa Srivastava
-GitHub: https://github.com/getRicha
-Twitter: https://twitter.com/gettRicha
-LinkedIn: https://linkedin.com/in/richasri
-`);
+const name=chalk.blue('Name')+': '+chalk.green('Richa Srivastava');
+const github=chalk.blue('Github')+': '+chalk.green('https://github.com/getRicha');
+const twitter=chalk.blue('Twitter')+': '+chalk.green('https://twitter.com/gettRicha');
+const lin=chalk.blue('LinkedIn')+': '+chalk.green('https://linkedin.com/in/richasri');
+
+const box= {
+    padding: 1,
+    margin: 1,
+    borderStyle: 'round',
+    borderColor: 'green',
+};
+
+const msg=boxen(`${name}\n${github}\n${twitter}\n${lin}`,box);
+
+console.log(msg);
