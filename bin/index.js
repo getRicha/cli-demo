b893a32105bebbd9e3b9cc9c8fabd1ee74be9b64
getRicha <richasri2002@gmail.com>
2022-01-19 23:09:31 +0530

    Initial commit

---
+++ bin/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+console.log(`Name: Richa Srivastava
+GitHub: https://github.com/getRicha
+Twitter: https://twitter.com/gettRicha
+LinkedIn: https://linkedin.com/in/richasri
+`);
